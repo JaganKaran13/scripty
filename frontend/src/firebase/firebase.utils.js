20c176cfa85fe97b82026c30a699464023e9e4b4
rajcrk <rajkumar.cind@gmail.com>
2019-10-11 20:20:53 +0530

    google api key

--- frontend/src/firebase/firebase.utils.js
+++ frontend/src/firebase/firebase.utils.js
@@ -3,9 +3,9 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const config = {
-    apiKey: "AIzaSyCD_orGXfpjqB4Bm9f5fMAebXmqXLBRvP0",
-    authDomain: "crwn-db-3.firebaseapp.com",
-    databaseURL: "https://crwn-db-3.firebaseio.com",
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: "crwn-db-3",
     storageBucket: "",
     messagingSenderId: "639262688271",
